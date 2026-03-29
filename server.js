0b9812b7d74c328c5b84f5de5a16a8782aeb5aeb
dbourret <57369462+dbourret@users.noreply.github.com>
2026-03-29 18:59:53 -0400

    Remove default PORT fallback in server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const crypto = require("crypto");
 require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
