a334541e2aabfbda6446fdd8fa0e57171f25a680
dbourret <dan@djbourret.com>
2026-04-16 16:58:09 -0400

    server.js

--- nodejs/server.js
+++ nodejs/server.js
@@ -4,7 +4,7 @@ const { fulfillOrderWithWhcc, getWhccAccessToken } = require("./whcc");
 
 console.log("DB_HOST =", process.env.DB_HOST);
 console.log("DB_USER =", process.env.DB_USER);
-console.log("DB_PASS exists =", !!process.env.DB_PASS);
+console.log("DB_PASSWORD exists =", !!process.env.DB_PASSWORD);
 console.log("DB_NAME =", process.env.DB_NAME);
 
 console.log("SMTP_HOST =", process.env.SMTP_HOST);
